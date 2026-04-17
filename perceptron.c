a81f666624d671e8c715512b37402c030ded57ba
abrahamlincoln <rudraastro05@gmail.com>
2026-04-18 00:21:04 +0530

    Fixed concepts

--- perceptron.c
+++ perceptron.c
@@ -3,24 +3,40 @@
 typedef struct perceptron
 {
     int bias;
-    int input1;
-    int input2;
-    int output;
+    int num_of_inputs;
+    int* weights;
     struct perceptron* next;
 }perceptron;
-perceptron* init_basic_perceptron(int bias,int input1,int input2)
+perceptron* init_basic_perceptron(int bias,int n,int* weight)
 {
     perceptron* temp=malloc(sizeof(perceptron));
     temp->bias=bias;
-    temp->input1=input1;
-    temp->input2=input2;
-    temp->output=(input1+input2+bias)<=0?0:1;
+    temp->num_of_inputs=n;
+    temp->weights=malloc(sizeof(int)*temp->num_of_inputs);
+    for(int i=0;i<n;i++)
+    {
+        temp->weights[i]=weight[i];
+    }
     temp->next=NULL;
     return temp;
 }
+perceptron* nand_gate()
+{
+    int weights[2]={-2,-2};
+    return init_basic_perceptron(3,2,weights);
+}
+int run_basic_perceptron(perceptron* temp,int* input, int n)
+{
+    int x=0;
+    for(int i=0;i<n;i++)
+    {
+        x+=(temp->weights[i]*input[i]);
+    }
+    return (x+(temp->bias))<=0?0:1;
+}
 int main()
 {
-    perceptron* temp=init_basic_perceptron(3,-2,-2);
-    printf("%d",temp->output);
+    int array[2]={1,1};
+    printf("%d",run_basic_perceptron(nand_gate(),array,2));
     return 0;
 }
