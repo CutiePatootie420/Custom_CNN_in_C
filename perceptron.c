ab70215249a4c00b2932888fb252781ae9325092
abrahamlincoln <rudraastro05@gmail.com>
2026-04-23 16:48:54 +0530

    Robust bit addition circuit using perceptrons as nand gates

--- perceptron.c
+++ perceptron.c
@@ -1,71 +1,152 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define BIT_ADDER_SIZE 7
 typedef struct perceptron
 {
+    int info;
     double bias;
-    double temp_sum;
-    struct perceptron* next;
+    int in_degree;
+    int in_degree_copy;
+    int next_count;
+    struct perceptron** next_arr;
+    double* weights;
+
 }perceptron;
-perceptron* init_basic_perceptron(double bias)
+typedef struct queue
 {
-    perceptron* temp=malloc(sizeof(perceptron));
-    temp->bias=bias;
-    temp->temp_sum=0;
-    temp->next=NULL;
+    int size;
+    struct perceptron** arr;
+    int front;
+    int rear;
+}queue;
+queue* init_queue(int size)
+{
+    queue* temp=malloc(sizeof(queue));
+    temp->size=size;
+    temp->arr=malloc(sizeof(struct perceptron*)*size);
+    temp->front=0;
+    temp->rear=0;
     return temp;
 }
-void run_basic_perceptron(double weight,double input, perceptron* temp)
+void enqueue(queue* q, perceptron* x)
 {
-    temp->temp_sum+=(weight*input);
+    q->arr[q->rear]=x;
+    q->rear=(q->rear+1)%q->size;
 }
-void fire_perceptron(double weight,perceptron* curr)
+perceptron* dequeue(queue* q)
 {
-    int output;
-    if(curr->temp_sum+curr->bias>0)
-    {
-        output=1;
-    }
-    else
-    {
-        output=0;
-    }
-    run_basic_perceptron(weight,output,curr->next);
+    perceptron* x=q->arr[q->front];
+    q->front=(q->front+1)%q->size;
+    return x;
 }
-void bit_addition(int x1, int x2,int* sum, int* carry)
+int is_empty(queue* q)
 {
-    double bias=3;
-    double w=-2;
-    perceptron* out1=init_basic_perceptron(bias);
-    run_basic_perceptron(w,x1,out1);
-    run_basic_perceptron(w,x2,out1);
-    perceptron* out2=init_basic_perceptron(bias);
-    out1->next=out2;
-    run_basic_perceptron(w,x1,out2);
-    fire_perceptron(w,out1);
-
-    perceptron* out3=init_basic_perceptron(bias);
-    run_basic_perceptron(w,x2,out3);
-    out1->next=out3;
-    fire_perceptron(w,out1);
-    perceptron* out4=init_basic_perceptron(bias);
-    out2->next=out4;
-    fire_perceptron(w,out2);
-    out3->next=out4;
-    fire_perceptron(w,out3);
-    perceptron* out5=init_basic_perceptron(bias);
-    out1->next=out5;
-    fire_perceptron(w,out1);
-    fire_perceptron(w,out1);
-    *sum=(out4->temp_sum+out4->bias)>0?1:0;
-    *carry=(out5->temp_sum+out5->bias)>0?1:0;
+    if(q->rear==q->front)
+    {
+        return 1;
+    }
+    return 0;
+}
+void compute_output(perceptron* p)
+{
+    p->info=((p->info+p->bias)<=0)?0:1;
+}
+void fire_perceptron(perceptron* p)
+{
+    for(int i=0;i<p->next_count;i++)
+    {
+        p->next_arr[i]->info+=p->info*p->weights[i];
+    }
 }
 int main()
 {
-    int x1=0;
-    int x2=0;
-    int sum;
-    int carry;
-    bit_addition(x1,x2,&sum,&carry);
-    printf("%d %d",sum,carry);
-    return 0;
+    perceptron* bit_adder_graph=malloc(sizeof(perceptron)*BIT_ADDER_SIZE);
+    int sizes[]={2,2,4,1,1,0,0};
+    for(int i=0;i<BIT_ADDER_SIZE;i++)
+    {
+        bit_adder_graph[i].info=0;
+        bit_adder_graph[i].next_count=sizes[i];
+        bit_adder_graph[i].next_arr=malloc(sizeof(perceptron*)*bit_adder_graph[i].next_count);
+        bit_adder_graph[i].weights=malloc(sizeof(double)*bit_adder_graph[i].next_count);
+        bit_adder_graph[i].bias=3;
+        bit_adder_graph[i].in_degree=0;
+        bit_adder_graph[i].in_degree_copy=0;
+        for(int j=0;j<bit_adder_graph[i].next_count;j++)
+        {
+            bit_adder_graph[i].weights[j]=-2;
+        }
+    }
+
+    bit_adder_graph[0].next_arr[0]=&bit_adder_graph[2];
+    bit_adder_graph[0].next_arr[1]=&bit_adder_graph[3];
+    bit_adder_graph[1].next_arr[0]=&bit_adder_graph[2];
+    bit_adder_graph[1].next_arr[1]=&bit_adder_graph[4];
+    bit_adder_graph[2].next_arr[0]=&bit_adder_graph[3];
+    bit_adder_graph[2].next_arr[1]=&bit_adder_graph[4];
+    bit_adder_graph[2].next_arr[2]=&bit_adder_graph[6];
+    bit_adder_graph[2].next_arr[3]=&bit_adder_graph[6];
+    bit_adder_graph[3].next_arr[0]=&bit_adder_graph[5];
+    bit_adder_graph[4].next_arr[0]=&bit_adder_graph[5];
+
+    for(int i=0;i<BIT_ADDER_SIZE;i++)
+    {
+        for(int j=0;j<bit_adder_graph[i].next_count;j++)
+        {
+            bit_adder_graph[i].next_arr[j]->in_degree++;
+            bit_adder_graph[i].next_arr[j]->in_degree_copy++;
+        }
+    }
+
+    perceptron** sorted_arr=malloc(sizeof(perceptron*)*BIT_ADDER_SIZE);
+    int index=0;
+    queue* q=init_queue(BIT_ADDER_SIZE+1);    
+    for(int i=0;i<BIT_ADDER_SIZE;i++)
+    {
+        if(bit_adder_graph[i].in_degree==0)
+        {
+            enqueue(q,&bit_adder_graph[i]);
+        }
+    }
+    int x;
+    perceptron* curr;
+    while(!is_empty(q))
+    {
+        curr=dequeue(q);
+        sorted_arr[index++]=curr;
+        for(int i=0;i<curr->next_count;i++)
+        {
+            x=--(curr->next_arr[i]->in_degree);
+            if(x==0)
+            {
+                enqueue(q,curr->next_arr[i]);
+            }
+        }
+    }
+    bit_adder_graph[0].info=0; //set input1
+    bit_adder_graph[1].info=0; //set input1
+
+    for(int i=0;i<index;i++)
+    {
+        if(sorted_arr[i]->in_degree_copy!=0)
+        {
+            compute_output(sorted_arr[i]);
+        }
+        fire_perceptron(sorted_arr[i]);
+    }
+
+    for(int i=0;i<index;i++) //check which index is giving sum and carry
+    {
+        printf("%ld ", sorted_arr[i] - bit_adder_graph);
+    }
+    printf("\nsum: %d, carry: %d",sorted_arr[6]->info,sorted_arr[5]->info);
+
+    free(sorted_arr);
+    free(q->arr);
+    free(q);
+    for(int i=0;i<BIT_ADDER_SIZE;i++)
+    {
+        free(bit_adder_graph[i].next_arr);
+        free(bit_adder_graph[i].weights);
+    }
+    free(bit_adder_graph);
 }
